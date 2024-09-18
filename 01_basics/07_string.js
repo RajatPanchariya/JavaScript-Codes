bb89968b4ea517b0e315862170ed1b5b69c7d005
RajatPanchariya <112881549+RajatPanchariya@users.noreply.github.com>
2024-09-19 01:08:15 +0530

    Create 07_string.js

---
+++ 01_basics/07_string.js
@@ -0,0 +1,30 @@
+const gameName = new String('harsidhi-haweli-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "harsidhihaweli%20com"
+
+// console.log(url.replace('%20', '.'))
+// console.log(url.includes('haweli'))
+// console.log(gameName.split('-'));
+
+let name = "Rajat Panchariya"
+let mail = "rajatpanchariya2004@gmail.com"
+console.log(`My name is ${name} and my mail id is ${mail}`);
