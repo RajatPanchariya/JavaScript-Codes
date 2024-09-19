548ebef8164bd1923d2db467f1fd5ce57f8b5798
RajatPanchariya <112881549+RajatPanchariya@users.noreply.github.com>
2024-09-19 18:42:46 +0000

    number and math

---
+++ 01_basics/08_number-and-math.js
@@ -0,0 +1,32 @@
+const score= 400
+const balacnce = new Number(100)
+// console.log(balacnce);
+// console.log(score);
+
+// console.log(balacnce.toFixed(2))
+// console.log(balacnce.toString().length);
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math.min(4,2,5,8,3,6,4));
+// console.log(Math.max(4,2,5,8,3,6,4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.abs(-4));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
